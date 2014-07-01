04a0405fd1701121e9fa1c8efdfbcaf31f91adb7
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-07-01 15:14:18 -0700

    fixed api tokens

--- src/test/java/com/bitvault/AccountCollectionTest.java
+++ src/test/java/com/bitvault/AccountCollectionTest.java
@@ -7,7 +7,7 @@ import org.junit.*;
 public class AccountCollectionTest {
 	
 	private static final String appUrl = "http://bitvault.pandastrike.com/apps/1GSHP19iLGjnbpJpv03FVA";
-	private static final String apiToken = "qVFS--Qv47juqT0soSuig1KaRTqVh78BRR4Ws8bvl2g";
+	private static final String apiToken = "v4WuaI1DhE0u4iazmo0E1z7YiOS1bYqnIOVKInnxyWo";
 	
 	private static Client client = new Client(appUrl, apiToken);
 	
