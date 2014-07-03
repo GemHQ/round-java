44b65c5978132f02bfc9ca85bdf46e1d3267d169
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-07-03 10:54:17 -0700

    token change again

--- src/test/java/com/bitvault/ClientTest.java
+++ src/test/java/com/bitvault/ClientTest.java
@@ -7,7 +7,7 @@ import org.junit.*;
 public class ClientTest {
 
   private static final String appUrl = "http://bitvault.pandastrike.com/apps/1GSHP19iLGjnbpJpv03FVA";
-  private static final String apiToken = "v4WuaI1DhE0u4iazmo0E1z7YiOS1bYqnIOVKInnxyWo";
+  private static final String apiToken = "jQ4LsU2_jRZwEx6T51JfGcnKpNVOIoxwXKE7WAiqcUQ";
 
   private static Client client = new Client(appUrl, apiToken);
 
