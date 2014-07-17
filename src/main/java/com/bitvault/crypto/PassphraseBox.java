f722072039788e33cba594cf6fa9547c7d1e2277
Shreya1407 <shreya1407@gmail.com>
2014-07-17 14:11:38 -0700

    Changes byte to String

--- src/main/java/com/bitvault/crypto/PassphraseBox.java
+++ src/main/java/com/bitvault/crypto/PassphraseBox.java
@@ -1,5 +1,6 @@
 package com.bitvault.crypto;
 import java.io.*;
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
@@ -12,6 +13,7 @@ import static org.abstractj.kalium.NaCl.sodium;
 
 import org.abstractj.kalium.crypto.Random;
 
+import static org.abstractj.kalium.NaCl.Sodium.XSALSA20_POLY1305_SECRETBOX_NONCEBYTES;
 public class PassphraseBox {
 	
     byte[] salt;
@@ -19,9 +21,10 @@ public class PassphraseBox {
 	int iterations;
 	byte[] key;
 	int iterat = 100000;
-	byte[] ciphertext;
+	String ciphertext;
 	String passphrase;
 	SecretBox  box;
+	String plaintext;
 
 	public PassphraseBox(String p, byte[] s, int i)
 	{
@@ -32,6 +35,24 @@ public class PassphraseBox {
         //this.ciphertext =c;
 		
 	}
+	
+	public PassphraseBox(String p)
+	{
+		this.passphrase = p;
+	}
+	
+	private static String toHex(byte[] array) throws NoSuchAlgorithmException
+    {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
+    }
 	
 	public void initialize (String passphrase, byte[] salt, int iterations)throws NoSuchAlgorithmException , InvalidKeySpecException
 	{
@@ -55,16 +76,30 @@ public class PassphraseBox {
 		box = new SecretBox(key);
 	}
 	
-	public void decrypt(String passphrase, byte[] salt, byte[] nonce, int iterations, byte[] ciphertext)
+	public void decrypt(String passphrase, byte[] salt, byte[] nonce, int iterations, String ciphertext)
 	{
 		PassphraseBox box = new PassphraseBox(passphrase, salt, iterations);
 		box.decrypt(nonce, ciphertext);
 	}
 	
-	public void decrypt(byte[] nonce, byte[] ciphertext)
+	public void decrypt(byte[] nonce, String ciphertext)
 	{
 		box.decrypt(nonce, ciphertext);
 	}
 	
+	public void encrypt(byte[] plaintext, String passphrase)
+	{
+		PassphraseBox box = new PassphraseBox(passphrase);
+		box.encrypt(plaintext);
+	}
+	public void encrypt(byte[] plaintext)
+	{
+		Random r = new Random();
+		this.nonce = r.randomBytes(XSALSA20_POLY1305_SECRETBOX_NONCEBYTES);
+		this.ciphertext = box.encrypt(nonce, plaintext);
+		
+		this.salt = toHex(salt);
+		
+	}
 	
 }
