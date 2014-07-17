59cd25417d3cf02cf566955a898c6a624c591ce2
ruchasamant <rsamant@usc.edu>
2014-07-17 15:17:47 -0700

    test case crypto

--- src/main/java/com/bitvault/crypto/PassphraseBox.java
+++ src/main/java/com/bitvault/crypto/PassphraseBox.java
@@ -14,9 +14,10 @@ import static org.abstractj.kalium.NaCl.sodium;
 import org.abstractj.kalium.crypto.Random;
 
 import static org.abstractj.kalium.NaCl.Sodium.XSALSA20_POLY1305_SECRETBOX_NONCEBYTES;
+
 public class PassphraseBox {
 	
-    byte[] salt;
+    String salt;
 	byte[] nonce;
 	int iterations;
 	byte[] key;
@@ -26,7 +27,7 @@ public class PassphraseBox {
 	SecretBox  box;
 	String plaintext;
 
-	public PassphraseBox(String p, byte[] s, int i)
+	public PassphraseBox(String p, String s, int i)
 	{
 		this.passphrase=p;
 		this.salt= s;
@@ -54,17 +55,17 @@ public class PassphraseBox {
         }
     }*/
 	
-	public void initialize (String passphrase, byte[] salt, int iterations)throws NoSuchAlgorithmException , InvalidKeySpecException
+	public void initialize (String passphrase, String salt, int iterations)throws NoSuchAlgorithmException , InvalidKeySpecException
 	{
 	
 		Random r = new Random();
 		
-		PBEKeySpec spec = new PBEKeySpec(null, this.salt, this.iterations, 64 * 8);
+		PBEKeySpec spec = new PBEKeySpec(passphrase.toCharArray(), salt.getBytes(), iterations, (64 * 8));
 	    SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 	    this.key = skf.generateSecret(spec).getEncoded();
 	     
 		if(salt==null)
-			this.salt=r.randomBytes(16);
+			this.salt=(r.randomBytes(16)).toString();
 		else
 			this.salt=salt;
 		
@@ -73,18 +74,20 @@ public class PassphraseBox {
 		else 
 			this.iterations=iterations;
 		
-		box = new SecretBox(key);
+		box = new SecretBox(this.key);
 	}
 	
-	public void decrypt(String passphrase, byte[] salt, byte[] nonce, int iterations, String ciphertext)
+	public String decrypt(String passphrase, String salt, String nonce, int iterations, String ciphertext)
 	{
 		PassphraseBox box = new PassphraseBox(passphrase, salt, iterations);
-		box.decrypt(nonce, ciphertext);
+		String s=box.decrypt(nonce, ciphertext);
+		return s;
 	}
 	
-	public void decrypt(byte[] nonce, String ciphertext)
+	public String decrypt(String nonce, String ciphertext)
 	{
-		box.decrypt(nonce, ciphertext.getBytes());
+		String s= (box.decrypt(nonce.getBytes(), ciphertext.getBytes())).toString();
+		return s;
 	}
 	
 	public void encrypt(String plaintext, String passphrase)
--- src/test/java/com/bitvault/crypto/PassphraseBoxTest.java
+++ src/test/java/com/bitvault/crypto/PassphraseBoxTest.java
@@ -1,14 +1,23 @@
 package com.bitvault.crypto;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 import org.junit.*;
 
 public class PassphraseBoxTest {
 	private static final String cipherText = "652f0af6af29e4d643b2fc26533c39591aa9a67cac961ade23330edde8ddec8b6124c2a43a658e8f5086c3df010668ec97534902da313b9d4f9a95bdd5162dd94faaaa48577d31c5911b22ef7ed06da5f2194804827b18a482ded1e754b1ec2886be311ddc1a388a5a15e605da980e4d22684b1433f256d0bb078f93d64226";
 	private static final String nonce = "6f338e562e0defee6d74d87f199bad098f202fe788437f66";
 	private static final String salt = "f554541cc823b3593a160c5ac269526f";
+	private static final String passphrase = "passphrase";
 	private static final String clearText = "tprv8ZgxMBicQKsPf4MukJZwUZSQhgrRP6SGXYh2Ds2hnAH5t6wKJtYq1ABc534anpFQSzYTY6j3C5PW8dngf7RLRPQsQLbCQcTv4LGUQZKmcUu";
+	private static final int iterations = 100000;
 	
-	@Test public void decryptAddressTest() {
+	@Test public void decryptAddressTest() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		PassphraseBox pass = new PassphraseBox(passphrase,salt,iterations);
+	    pass.initialize(passphrase, salt, iterations);
+		String s=pass.decrypt(passphrase, salt, nonce, iterations, cipherText);
+		Assert.assertEquals(s, clearText);
 		
 	}
 }
