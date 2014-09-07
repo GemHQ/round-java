65fae227f1ae5cb796abc6021dae204a5b84706a
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-09-07 15:42:02 -0700

    added created at to transactions

--- src/main/java/com/bitvault/Transaction.java
+++ src/main/java/com/bitvault/Transaction.java
@@ -4,10 +4,15 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Transaction extends Resource {
 	
 	public static final String RESOURCE_NAME = "transaction";
+    public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
 
 	public Transaction(JsonObject resource, Client client) {
 		super(resource, client, RESOURCE_NAME);
@@ -49,18 +54,31 @@ public class Transaction extends Resource {
         }
 
         if (getType().equals("outgoing")) {
-            String address = null;
             for (JsonElement element : bitcoinTransaction.getAsJsonArray("outputs")) {
                 JsonObject output = element.getAsJsonObject();
                 JsonObject metadata = output.getAsJsonObject("metadata");
-                address = output.get("address").getAsString();
+                if (Math.abs(output.get("value").getAsLong()) != Math.abs(getValue()))
+                    continue;
+
+                String address = output.get("address").getAsString();
                 if (metadata.has("pay_to_email"))
                     return metadata.get("pay_to_email").getAsString();
+                return address;
             }
-
-            return address;
         }
 
         return null;
+    }
+
+    public Date getCreatedAt() {
+        String dateString = getBitcoinTransaction().get("created_at").getAsString();
+
+        Date createdAt = null;
+        try {
+            createdAt = DATE_FORMATTER.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return createdAt;
     }
 }
