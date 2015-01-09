ac478eb22ce8ee2a609593bdf8b3055ed887987d
Joshua Lederman <joshua@gem.co>
2015-01-09 08:12:17 -0800

    set wallet in account plus setup

--- src/main/java/co/gem/round/examples/WalletAndAccountOperations.java
+++ src/main/java/co/gem/round/examples/WalletAndAccountOperations.java
@@ -1,10 +1,9 @@
 package co.gem.round.examples;
 
-import co.gem.round.Account;
-import co.gem.round.Address;
-import co.gem.round.User;
-import co.gem.round.Wallet;
+import co.gem.round.*;
 import co.gem.round.patchboard.Client;
+import java.security.*;
+import java.security.spec.*;
 
 import java.io.IOException;
 
@@ -12,7 +11,7 @@ import java.io.IOException;
  * Created by jled on 12/31/14.
  */
 public class WalletAndAccountOperations {
-    public static void init() throws IOException, Client.UnexpectedStatusCodeException {
+    public static void init(Boolean makeAddress) throws IOException, Client.UnexpectedStatusCodeException {
         User authUser = UserDeviceAuth.init();
 
         Wallet myWallet = authUser.wallets().get("default");
@@ -20,23 +19,26 @@ public class WalletAndAccountOperations {
 
 
         Account myAccount = myWallet.accounts().get("default");
-        System.out.println(Long.toString(myAccount.balance()));
+        System.out.println("\nbalance: " + Long.toString(myAccount.balance()) +
+        "\nPending Balance: " + Long.toString(myAccount.pendingBalance()));
 
-        Address address = myAccount.addresses().create();
-        System.out.println(
-                "\nAddress: " + address.getAddressString());
+        if(makeAddress) {
+            Address address = myAccount.addresses().create();
+            System.out.println(
+                    "\nAddress: " + address.getAddressString());
+        }
 
         //fund the account from a faucet and wait for 6 confirmations on a tx before attempting to send
 
-//        Payment payment = null;
-//        try {
-//            payment = myAccount.payToAddress("password", "1lJPij0u4209430jsdf", 84059540L);
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        } catch (InvalidKeySpecException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println(payment.getStatus());
+        Payment payment = null;
+        try {
+            payment = myAccount.payToAddress("password", "mwhRqLNwRK7jsBLETP8ears5uSR7F7kEMN", 247644L);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(payment.getStatus());
     }
 }
