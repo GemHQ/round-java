9689e3c5048cbfc781801061ccdac43d30744b8d
Joshua Lederman <joshua@gem.co>
2015-03-05 09:46:42 -0800

    wallet JavaDoc

--- src/main/java/co/gem/round/Wallet.java
+++ src/main/java/co/gem/round/Wallet.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+/**
+ * Wallet is a gem wallet which is the HD Multi-Sig (2of3) wallet that is owned by a User.  Wallets provide access to
+ * accounts in the wallet.  As well as info such as the balance of all accounts.
+ * @author Julian Del Vergel de Dios (julian@gem.co) on 12/18/14.
+ * @see co.gem.round.Account
+ */
 public class Wallet extends Base {
 
   private EncryptedMessage encryptedSeed;
@@ -40,12 +46,22 @@ public class Wallet extends Base {
     callback.execute(wallet);
   }
 
+  /**
+   * Getter for accounts in a wallet
+   * @return AccountCollection of Accounts
+   * @throws Client.UnexpectedStatusCodeException
+   * @throws IOException
+   */
   public AccountCollection accounts() throws Client.UnexpectedStatusCodeException, IOException {
     AccountCollection accounts = new AccountCollection(resource.subresource("accounts"), this.round, this);
     accounts.fetch();
     return accounts;
   }
 
+  /**
+   * Getter for the elements of the encrypted primary seed.
+   * @return EncryptedMessage containing ciphertext, salt, nonce, iterations for decryption purposes
+   */
   public EncryptedMessage getEncryptedSeed() {
     if (this.encryptedSeed == null) {
       JsonObject seedObject = getObject("primary_private_seed");
@@ -61,22 +77,40 @@ public class Wallet extends Base {
     return this.encryptedSeed;
   }
 
+  /**
+   * Getter for the backup pubSeed
+   * @return String
+   */
   public String getBackupPublicSeed() {
     return getString("backup_public_seed");
   }
 
+  /**
+   * Getter for Gem API's cosigner pubSeed
+   * @return String
+   */
   public String getCosignerPublicSeed() {
     return getString("cosigner_public_seed");
   }
 
+  /**
+   * Getter for Primary pubSeed
+   * @return String
+   */
   public String getPrimaryPublicSeed() {
     return getString("primary_public_seed");
   }
 
+  /**
+   * Sum of all of the account balances within a wallet.  Balances are based off of the value of inputs with 6 or more
+   * confirmations.
+   * @return Long
+   */
   public Long balance() {
     return getLong("balance");
   }
 
+  @Deprecated
   public static class Wrapper {
     public Wallet wallet;
     public String backupPrivateSeed;
